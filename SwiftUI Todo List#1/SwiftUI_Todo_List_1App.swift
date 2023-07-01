940cb849bda7bfd129f90a0ab2f05f91111d7104
Pablo <pawellreshet@icloud.com>
2023-07-01 11:41:34 +0300

    Initial Commit

---
+++ SwiftUI Todo List#1/SwiftUI_Todo_List_1App.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUI_Todo_List_1App.swift
+//  SwiftUI Todo List#1
+//
+//  Created by Pavel Reshetov on 01/07/2023.
+//
+
+import SwiftUI
+
+/*
+ MVVM Architecture
+ 
+ Model - data point
+ View - UI
+ ViewModel - manages Models for View
+ */
+
+@main
+struct SwiftUI_Todo_List_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
